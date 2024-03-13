ceac376020e9242ac13d3d26a44e52eda09d16a1
moves <movesnues@gmail.com>
2024-03-13 09:38:23 +0300

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + ' ' + firstName + ' ' + middleName;
+        System.out.println("Ф.И.О. сщтрудника - " + fullName);
+        String unt = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + unt);
+
+
+        String fullName2 = "Иванов Семён Семёнович";
+        String fullName22 = fullName2.replace("ё", "е");
+        System.out.println(fullName22);
     }
 }
